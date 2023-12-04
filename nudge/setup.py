21c70b76235b2da656502e0da0bfbdc5a1f64570
MaggiR <mark@rothermel.me>
2023-12-04 10:58:59 +0100

    Add setup.py

---
+++ nudge/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("../requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.readlines()
+
+setup(
+    name='nudge',
+    version='0.1.0',
+    author='Hikaru Shindo',
+    author_email='hikisan.gouv',
+    packages=find_packages(),
+    include_package_data=True,
+    # package_dir={'': 'nudge'},
+    url='tba',
+    description='Neurally gUided Differentiable loGic policiEs (NUDGE)',
+    long_description=long_description,
+    install_requires=requirements,
+)
