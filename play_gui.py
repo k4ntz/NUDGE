92606e8dcc92628763afe9c37ee5464ee39adccf
Quentin Delfosse <q.delfosse@gmail.com>
2024-06-25 10:56:32 +0200

    Added gpt4-o

--- play_gui.py
+++ play_gui.py
@@ -1,7 +1,14 @@
 from nudge.renderer import Renderer
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("--agent_path", type=str, default="out/runs/seaquest/logic/")
+
+
+
 
 if __name__ == "__main__":
-    renderer = Renderer(agent_path="out/runs/seaquest/logic/24-04-04-16-56",
+    renderer = Renderer(agent_path="out/runs/seaquest/logic/",
                         fps=15,
                         deterministic=False,
                         env_kwargs=dict(render_oc_overlay=True),
